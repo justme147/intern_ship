1aff5e24609b6170e859e167b04ad3dfb449dad1
justme147 <sotin1996@gmail.com>
2020-03-15 14:44:55 +0400

    rewrote Menu screen

--- src/scripts/startscreenmenu.js
+++ src/scripts/startscreenmenu.js
@@ -2,13 +2,6 @@ import * as $ from "jquery";
 
 $(document).ready(function() {
   $(".burger").on("click", () => {
-    $(".sidebar").toggleClass("sidebar-menu");
-    $(".sidebar__inner").toggleClass("sidebar__inner-menu");
-    $(".sidebar__inner .burger").toggleClass("burger-active");
-    $(".menu").toggleClass("menu-display");
-    $(".sidebar-lang-switch").toggleClass("sidebar-lang-switch__display");
-    $(".body").toggleClass("body-menu");
-    $(".body-header-burger").toggleClass("body-header-burger__display");
-    $(".slider").toggleClass("slider-menu");
+    $(".container__menu").fadeToggle(200);
   });
 });
